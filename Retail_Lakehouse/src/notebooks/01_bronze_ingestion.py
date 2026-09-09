83b371ac8026a6f5d6645c5244b504afd6b4844e
sanketazure19@gmail.com <sanketazure19@gmail.com>
2026-09-09 10:44:50 +0000

    fix_bronze_ingest

--- Retail_Lakehouse/src/notebooks/01_bronze_ingestion.py
+++ Retail_Lakehouse/src/notebooks/01_bronze_ingestion.py
@@ -1,4 +1,8 @@
 # Databricks notebook source
+# /// script
+# [tool.databricks.environment]
+# environment_version = "5"
+# ///
 import inspect
 import retail_lakehouse
 import retail_lakehouse.utils.quality as quality_module
